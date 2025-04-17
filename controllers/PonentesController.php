aaa35b4f926a5e53f26007de0a09d372e7d79b3d
juan david barbosa neira <you@example.com>
2025-04-17 11:24:50 -0500

    Formulario Registro Ponentes

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -7,8 +7,14 @@ use MVC\Router;
 class PonentesController {
     public static function index(Router $router) {
         $router->render('admin/ponentes/index', [
-            'titulo' => 'Ponentes /  Conferencistas'
-            
+            'titulo' => 'Ponentes /  Conferencistas'          
+        ]);
+    }
+    public static function crear(Router $router) {
+        $alertas = [];
+        $router->render('admin/ponentes/crear', [
+            'titulo' => 'Registrar Ponente',
+            'alertas' => $alertas          
         ]);
     }
 }
