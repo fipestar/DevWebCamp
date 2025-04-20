04f595077b44851c35e50e26a5f095bffff6241f
juan david barbosa neira <you@example.com>
2025-04-20 12:38:31 -0500

    Modelo y BD Ponentes

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,28 +1,31 @@
-(function(){
-    
-    const tagsInput = document.querySelector('#tags__input');
+(function() {
+
+    const tagsInput = document.querySelector('#tags__input')
 
     if(tagsInput) {
 
         const tagsDiv = document.querySelector('#tags');
-        const tagsInputHidden = document.querySelector('[name="tags]');
+        const tagsInputHidden = document.querySelector('[name="tags"]');
 
         let tags = [];
 
-        //Escuchar los cambios en el input
-        tagsInput.addEventListener('keypress', guardarTag);
+        // Recuperar del input oculto
+        if(tagsInputHidden.value !== '') {
+            tags = tagsInputHidden.value.split(',');
+            mostrarTags();
+        }
+ 
+        // Escuchar los cambios en el input
+        tagsInput.addEventListener('keypress', guardarTag)
 
         function guardarTag(e) {
-            
             if(e.keyCode === 44) {
-                if(e.target.value.trim() === '' || e.target.value < 1){
-                    return;
+                if(e.target.value.trim() === '' || e.target.value < 1) { 
+                    return
                 }
-                    e.preventDefault();
-
+                e.preventDefault();
                 tags = [...tags, e.target.value.trim()];
                 tagsInput.value = '';
-                
                 mostrarTags();
             }
         }
@@ -31,23 +34,23 @@
             tagsDiv.textContent = '';
             tags.forEach(tag => {
                 const etiqueta = document.createElement('LI');
-                etiqueta.classList.add('formulario__tag');
+                etiqueta.classList.add('formulario__tag')
                 etiqueta.textContent = tag;
-                etiqueta.ondblclick = eliminarTag;
-                tagsDiv.appendChild(etiqueta);
+                etiqueta.ondblclick = eliminarTag
+                tagsDiv.appendChild(etiqueta)
             })
             actualizarInputHidden();
-        }
+        }   
 
-        function eliminarTag(e){
-            e.target.remove();
-            tags = tags.filter(tag => tag !== e.target.textContent);
+        function eliminarTag(e) {
+            e.target.remove()
+            tags = tags.filter(tag => tag !== e.target.textContent)
             actualizarInputHidden();
         }
-        
 
         function actualizarInputHidden() {
-            tagsInputHidden.value = tags.toString();
+           tagsInputHidden.value = tags.toString();
         }
     }
-})()
+
+})();
